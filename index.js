12ae2e840ef4a237622d3348f2129143fe6e0ff5
Janne <janne@Jannes-MBP.Home>
2021-02-25 11:04:36 +0200

    muuutoksia taas

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-// using the http module
-let http = require('http'),
- 
-// look for PORT environment variable, 
-// else look for CLI argument,
-// else use hard coded value for port 8080
-port = process.ENV.PORT || process.argv[2] || 8080;
- 
-// create a simple server
-let server = http.createServer(function (req, res) {
- 
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        res.write('hello heroku!', 'utf-8');
-        res.end();
- 
-    });
- 
-// listen on the port
-server.listen(port, function () {
- 
-    console.log('app up on port: ' + port);
- 
-});
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
