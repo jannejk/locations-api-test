237e8fec1b0f83d5b7c2a1f2f2f4819d9bfa807f
Janne <janne@Jannes-MBP.Home>
2021-02-25 12:51:26 +0200

    muuutoksia taas v2

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
+const port = process.env.PORT || 3000;
+
+//kun http-pyyntö tulee, kutsu callback funkkaria
+app.use((req,res, next) => {
+    console.log("hello");
+    next();//kutsu seuraavaa middleware funkkaria
+})
+app.use((req,res, next) => {
+    console.log("world!");
+    next();//kutsu seuraavaa middleware funkkaria
+})
+app.use(express.static("public"));
+
+let db = [
+    {id: 1, latitude: 80, longitude: 80},
+    {id: 2, latitude: 90, longitude: 90},
+];
+app.get("/api/locations", (req, res) =>{
+    res.send(db);
+})
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
